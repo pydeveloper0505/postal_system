714d2878edef371e5f43a0c35ecb60127e166e09
Reaktiv <developer.py1505@gmail.com>
2026-04-12 08:21:24 +0500

    udpate

--- postal_system/settings.py
+++ postal_system/settings.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-me-in-production')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
